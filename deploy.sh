278a0212386cac7d77264f5984da77c03fe6b0d7
Kevin13590732645 <175273396@qq.com>
2026-01-17 20:04:52 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 1. 安装依赖
+pip install -r requirements.txt
+
+# 2. 构建
+mkdocs build
+
+# 3. 推送
+git add .
+git commit -m "Update content"
+git push origin main
+
+echo "部署完成！访问：https://你的用户名.github.io/OI-wiki/"
